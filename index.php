fbf0b0f929aa5135a9a7d48ac752866f7fdcee95
Server <server@server.com>
2013-09-12 17:38:48 -0700

    Adding changes

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta charset="UTF-8">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Seductive Turtle Gaming Community</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/bootstrap.css" type="text/css">
-    <link rel="stylesheet" href="./css/bootstrap-responsive.css" type="text/css">
-    <link rel="stylesheet" href="./css/strip.css" type="text/css">
-    <link rel="stylesheet" href="./css/strip-responsive.css" type="text/css">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Seductive Turtle Gaming Community</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="./css/bootstrap.css" type="text/css">
+	<link rel="stylesheet" href="./css/bootstrap-responsive.css" type="text/css">
+	<link rel="stylesheet" href="./css/strip.css" type="text/css">
+	<link rel="stylesheet" href="./css/strip-responsive.css" type="text/css">
    	<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,700,600" rel="stylesheet">
-    <link href='http://fonts.googleapis.com/css?family=Merriweather:400,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Merriweather:400,700' rel='stylesheet' type='text/css'>
 
 	<link rel="shortcut icon" href="/img/favico.png">
  <script type="text/javascript">
@@ -46,39 +46,59 @@ fb_param.currency = 'USD';
 <section class="container">
 	<div class="row">
 		<header class="highlight span12">
-			<h2>Welcome to the Seductive Turtle Gaming Comunity</h2>               
-		</header>        
+			<h2>Welcome to the Seductive Turtle Gaming Community</h2>
+		</header>		
 	</div>   
 	<div class="row">
 		<div class="offset3 span6">
 			<hr class="shadow-line"/>
 		</div>
-	</div>	    
-    <div class="row">
-	<header class="highlight span12">
-		<p>Unlike most gaming communities, we mostly hang out in our teamspeak server. Teamspeak is a voice chat client, like Ventrillo, or Mumble, but a little bit more powerful. There is a link to their website below, with a download after the link.</p>
-		<p>
-			<a href="http://www.teamspeak.com/" class="big-btn">Visit TeamSpeak.com</a>
-			<a href="ts3server://ts.st-gc.org/?port=9987" class="big-btn">Join the TeamSpeak</a>
-		</p>
-	</header>        
-	<h3 class="subhead">Hit up a moderator or an administrator for more info</h3>
-    </div>   
+	</div>		
+	<div class="row">
+		<header class="highlight span12">
+			<p>Unlike most gaming communities, we mostly hang out in our teamspeak server. Teamspeak is a voice chat client, like Ventrillo, or Mumble, but a little bit more powerful. There is a link to their website below, with a download after the link.</p>
+			<p>
+				<a href="http://www.teamspeak.com/" class="big-btn">Visit TeamSpeak.com</a>
+				<a href="ts3server://ts.st-gc.org/?port=9987" class="big-btn">Join the TeamSpeak</a>
+			</p>
+		</header>		
+		<h3 class="subhead">Hit up a moderator or an administrator for more info.</h3>
+	</div>
+	<div class="row">
+		<div class="offset3 span6">
+			<hr class="shadow-line"/>
+		</div>
+	</div>
+	<div class="row">
+		<h3 class="subhead">Even if you are already in a guild, a clan, or a group, we don't mind, you can still have fun with us!</h3>
+	</div>   
 </section>
 
 <section class="container">
 </section>
 
 <footer class="container footer-box">   
-    <hr class="shadow-line" />         
-    <nav class="links">             
-        <a href="/">Home</a>
-        <a href="ts3server://ts.st-gc.org/?port=9987">Teamspeak</a>
+	<hr class="shadow-line" />		 
+	<nav class="links">			 
+		<a href="/">Home</a>
+		<a href="ts3server://ts.st-gc.org/?port=9987">Teamspeak</a>
 	<a href="http://bf3-top100.com/"><img src="http://bf3-top100.com/button.php?u=aequasi" alt="Battlefield 3 Top100" border="0" /></a>
-    </nav>  
+	</nav>  
 </footer>
 <script src="./js/jquery-1.8.0.min.js"></script>
 <script src="./js/bootstrap.js"></script>
-
+<script type="text/javascript">
+  var _gauges = _gauges || [];
+  (function() {
+	var t   = document.createElement('script');
+	t.type  = 'text/javascript';
+	t.async = true;
+	t.id	= 'gauges-tracker';
+	t.setAttribute('data-site-id', '522f3cc0613f5d0ffa000072');
+	t.src = '//secure.gaug.es/track.js';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(t, s);
+  })();
+</script>
 </body>
 </html>
