756079955b17398aa24895b0764904f1856f4fe2
Server <server@server.com>
2013-11-12 13:16:33 -0800

    Adding stuff

---
+++ web/adsql/include/config.php
@@ -0,0 +1,23 @@
+<?php
+/* new line below starting in AdsQL 1.7.7:	*/
+if (!defined("HAVE_ADSQL_CONFIG")) {
+
+//define("DB_SERVER", "db.st-gc.org");
+define("DB_SERVER", "localhost");
+define("DB_USER", "csgoUser");
+define("DB_PASS", 'ajajaj3');
+define("DB_NAME", "cs-go");
+
+define("EMAIL_FROM_NAME", "STGC");
+define("EMAIL_FROM_ADDR", "no-reply@st-gc.org");
+define("EMAIL_WELCOME", true);
+
+define("COMMUNITYNAME", "Seductive Turtle Gaming Community");
+
+// What are your Admins called?
+define("ADMIN", "Admins");
+
+/* new line below starting in AdsQL 1.7.7	*/
+define("HAVE_ADSQL_CONFIG", true); }
+
+?>
